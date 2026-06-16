49fab98e7cda2fecfdd615adf06ccdb74d864dd7
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-06-16 12:15:10 -0700

    Time: 0 ms (100%), Space: 20 MB (81.84%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // like 714 two 1-D vectors to track costs and profits
-        vector<int> buy(prices.size());
-        vector<int> sell(prices.size());
-        // initialize buy and sell rate
-        buy[0] = prices[0];
-        sell[0] = 0;
+        int profit = 0; 
         for (int i = 1; i < prices.size(); i++) {
-            // buy options: do nothing (holds the previous bought option) or buy
-            // a new stock
-            buy[i] = min(buy[i - 1], prices[i] - sell[i - 1]);
-            // sell options, holds the stock, or sell a stock
-            sell[i] = max(sell[i - 1], prices[i] - buy[i - 1]);
-            cout << "at index   " << i << "     buy: " << buy[i]
-                 << "   sell: " << sell[i] << endl;
+            // capture every segment of profit growth 
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
         }
-        return sell[prices.size() - 1];
+        return profit;
     }
 };
