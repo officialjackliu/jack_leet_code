457e41b02aec9051a8cb602c93ed8dd39e337940
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-06-19 16:44:37 -0700

    Time: 0 ms (100%), Space: 9.4 MB (28.28%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, int> s_map; 
+        unordered_map<char, int> t_map; 
+        for (int i = 0; i < s.size(); i++) {
+            // insert s char to s_map if it does not exist
+            if (s_map.find(s[i]) == s_map.end()) {
+                s_map[s[i]] = i;
+            }
+            // do for t 
+            if (t_map.find(t[i]) == t_map.end()) {
+                t_map[t[i]] = i;
+            }
+            // check if two string are isomoprhic order of there character appearance are same 
+            if (t_map[t[i]] != s_map[s[i]]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
