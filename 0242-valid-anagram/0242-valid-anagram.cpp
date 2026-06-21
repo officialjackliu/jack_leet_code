afc81db7997b6e25d17ed6889846e31c79b903dc
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-06-21 14:33:00 -0700

    Time: 9 ms (7.06%), Space: 9.7 MB (75.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // base case
-        if (s.size() != t.size()) {
-            return false;
+        // one hashmap, one loop 
+        if (s.length() != t.length()) {
+            return false; 
         }
-        map<char, int> s_map;
-        map<char, int> t_map;
-        for (char ch : s) {
-            s_map[ch]++;
+        map<char, int> hash_map; 
+        for (char ch: s) {
+            hash_map[ch]++;
         }
-        for (char ch : t) {
-            t_map[ch]++;
-        }
-        if (s_map.size() != t_map.size()) {
-            return false;
-        }
-        auto s_it = s_map.begin();
-        auto t_it = t_map.begin();
-        for (; s_it != s_map.end() && t_it != t_map.end(); s_it++ , t_it++) {
-
-            if (s_it->first != t_it->first || s_it->second != t_it->second) {
+        for (char ch: t) {
+            if (hash_map.find(ch) == hash_map.end() || hash_map[ch] == 0) {
                 return false;
             }
+            hash_map[ch] = hash_map[ch] - 1;
         }
         return true;
     }
