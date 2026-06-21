b12617b1a5c071f45ae263c7dc0b9e9760f9fcd9
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-06-21 14:07:50 -0700

    Time: 11 ms (82.59%), Space: 84.4 MB (18.81%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(), costs.end());
-        int nums = 0; 
+        // count array algorithm 
+        // build the frequency array, find max cost first 
+        int max_cost = 0; 
         for (int cost: costs) {
-            coins -= cost; 
-            if (coins >= 0) {
-                nums++;
-            } else {
+            max_cost = max(cost, max_cost);
+        }
+        vector<int> counts(max_cost + 1, 0);
+        int size = max_cost +1;
+        // cout << "size of counts array is    " << size << endl; 
+        // record the frequency of e ice cream cost in counts array 
+        for (int i = 0; i < costs.size(); i++) {
+            
+            counts[costs[i]] += 1;
+            // cout << "fill element " << costs[i] << "    to index " << counts[costs[i]] << endl;
+        }
+        int purchases = 0; 
+        for (int cost = 0; cost < size; cost++) {
+            if (coins <= 0) {
                 break;
             }
+            int freq = counts[cost];
+            while (freq != 0) {
+                if (coins < cost) {
+                    break;
+                }
+                coins -= cost; 
+                purchases++;
+                freq--;
+            }
         }
-        return nums;
+        return purchases; 
     }
 };
