2920244112db21a7bf01c62ed10b451dca07ddee
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-06-23 17:49:03 -0700

    Time: 0 ms (100%), Space: 22.8 MB (68.6%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -12,13 +12,13 @@ public:
         if (ratings[0] > ratings[1]) {
             candies[0]++;
         }
-         cout << "at first index, current candy is " << candies[0] << "   total candies " << total_candies << endl;
+         // cout << "at first index, current candy is " << candies[0] << "   total candies " << total_candies << endl;
          // first loop only check vs left hand side neighbor, 
         for (int i = 1; i < candies.size(); i++) {
             if (ratings[i] > ratings[i-1]) {
                 candies[i] = candies[i-1] + 1;
             } 
-            cout << "at index " << i << "   current candy is " << candies[i] << "   total candies " << total_candies << endl;
+            // cout << "at index " << i << "   current candy is " << candies[i] << "   total candies " << total_candies << endl;
         }
         // second loop goes from right to left, check if ratings is greater than its right hand side neighbor, if it is, increment if 
         for (int i = ratings.size() - 2; i >= 0; i--) {
@@ -30,7 +30,7 @@ public:
         for (int candy: candies) {
             total_candies += candy;
         }
-         cout << "   total candies " << total_candies << endl;
+        //  cout << "   total candies " << total_candies << endl;
         return total_candies;
     }
 };
