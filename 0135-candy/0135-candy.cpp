fca36ab366e93c06ae32ac05d12b5ab520f04dcc
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-06-23 17:48:49 -0700

    Time: 769 ms (5.04%), Space: 23.1 MB (30.25%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        // base case 
+        if (ratings.size() == 1) {
+            return 1;
+        }
+        vector<int> candies(ratings.size(), 1);
+        int total_candies = 0;
+        // iterate through every ratings, assume to give 1 candy at the start, check ratings vs neighbor, if higher of one if the neighbor, give them more candy 
+        // special case for first element
+        if (ratings[0] > ratings[1]) {
+            candies[0]++;
+        }
+         cout << "at first index, current candy is " << candies[0] << "   total candies " << total_candies << endl;
+         // first loop only check vs left hand side neighbor, 
+        for (int i = 1; i < candies.size(); i++) {
+            if (ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            } 
+            cout << "at index " << i << "   current candy is " << candies[i] << "   total candies " << total_candies << endl;
+        }
+        // second loop goes from right to left, check if ratings is greater than its right hand side neighbor, if it is, increment if 
+        for (int i = ratings.size() - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i+1] + 1, candies[i]);
+            }
+        }
+
+        for (int candy: candies) {
+            total_candies += candy;
+        }
+         cout << "   total candies " << total_candies << endl;
+        return total_candies;
+    }
+};
