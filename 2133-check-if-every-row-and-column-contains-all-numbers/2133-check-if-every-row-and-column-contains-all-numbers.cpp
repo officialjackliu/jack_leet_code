6c3a0eda50111a18258f29d058b46984311f240c
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-07-03 22:56:50 -0700

    Time: 0 ms (100%), Space: 43.6 MB (59.83%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        unordered_set<int> s1, s2;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(s1.find(matrix[i][j])!=s1.end())
-                    return false;
-                else
-                    s1.insert(matrix[i][j]);
-                if(s2.find(matrix[j][i])!=s2.end())
-                    return false;
-                else
-                    s2.insert(matrix[j][i]);
+        int n = matrix.size();
+        // rows
+        for(int i = 0; i < n; i++) {
+            vector<int> arr(n, 0);
+            for(int j = 0; j < n; j++) {
+                int idx = matrix[i][j] - 1;
+                if(arr[idx] != 0) return false;
+                arr[idx]++;
             }
-            s1.clear();
-            s2.clear();
         }
+
+        // col
+        for(int i = 0; i < n; i++) {
+            // one col
+            vector<int> arr(n, 0);
+            for(int j = 0; j < n; j++) {
+                int idx = matrix[j][i] - 1;
+                if(arr[idx] != 0) return false;
+                arr[idx]++;
+            }
+        }
+
         return true;
     }
 };
