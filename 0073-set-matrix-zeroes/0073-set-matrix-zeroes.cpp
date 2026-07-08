7c649d71d548726498645e1cfc866ad0ed1d13cf
Jack Liu <148827887+officialjackliu@users.noreply.github.com>
2026-07-08 15:11:53 -0700

    Time: 7 ms (19.1%), Space: 21.1 MB (21.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,66 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool zeroinFirstCol = false;
-        for (int row = 0; row < matrix.size(); row++) {
-            if (matrix[row][0] == 0) zeroinFirstCol = true;
-            for (int col = 1; col < matrix[0].size(); col++) {
-                if (matrix[row][col] == 0) {
-                    matrix[row][0] = 0;
-                    matrix[0][col] = 0;
-                }
-            }
-        }
+        /*
+            find all 0 elements coordinate in the matrix,
+            iterate row by row, record columsn and rows need to be set to 0, fill those rows with 0 after the scan loop
+        */
 
-        for (int row = matrix.size() - 1; row >= 0; row--) {
-            for (int col = matrix[0].size() - 1; col >= 1; col--) {
-                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
-                    matrix[row][col] = 0;
+        unordered_set<int> rows;
+        unordered_set<int> columns;
+        for (int row = 0; row < matrix.size(); row++) {
+            for (int col = 0; col < matrix[0].size(); col++) {
+                if (matrix[row][col] == 0) {
+                    rows.insert(row);
+                    columns.insert(col);
                 }
             }
-            if (zeroinFirstCol) {
-                matrix[row][0] = 0;
+        }
+        // fill rows
+        for (int row: rows) {
+            fill(matrix[row].begin(), matrix[row].end(), 0);
+        }
+        // fill columns
+        for (int col: columns) {
+            for (int row = 0; row < matrix.size(); row++) {
+                matrix[row][col] = 0;
             }
         }
+        return;
     }
 };
+
+// // prefill row 0
+// for (int col = 0; col < matrix[0].size(); col++) {
+//     if (matrix[0][col] == 0) {
+//         columns.insert(col);
+//         // fill column
+//         for (int row = 0; row < matrix.size(); row++) {
+//             matrix[row][col] = 0;
+//         }
+//     }
+// }
+// // fill row
+// if (columns.empty() == false) {
+//     fill(matrix[0].begin(), matrix[0].end(), 0);
+// }
+// // fill rest of rows and columns
+// for (int row = 1; row < matrix.size(); row++) {
+//     bool fill_row = false;
+//     for (int col = 0; col < matrix[0].size(); col++) {
+//         // skip filled column
+//         if (columns.count(col) == 1) {
+//             continue;
+//         }
+//         if (matrix[row][col] == 0) {
+//             // needs fill row
+//             fill_row = true;
+//             columns.insert(col);
+//             for (int row = 0; row < matrix.size(); row++) {
+//                 matrix[row][col] = 0;
+//             }
+//         }
+//     }
+//     if (fill_row == true) {
+//         fill(matrix[row].begin(), matrix[row].end(), 0);
+//     }
